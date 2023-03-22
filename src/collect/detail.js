88b4df74b16bbcc1f4d03b5983586683d54a8c21
Manojdeore7 <manojddeore11>
2023-03-22 16:18:21 +0530

    Fragment is added

--- src/collect/detail.js
+++ src/collect/detail.js
@@ -1,12 +1,14 @@
 import "./detail.css";
-
+import React from "react";
 function ElementUser(props) {
   let str = ` ${props.name} ( max age is ${props.age}  ) `;
   console.log(str);
   return (
-    <div className="detail">
-      <div>{str}</div>
-    </div>
+    <React.Fragment>
+      <div div className="detail">
+        <div>{str}</div>
+      </div>
+    </React.Fragment>
   );
 }
 export default ElementUser;
